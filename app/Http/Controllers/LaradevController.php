856d343019b76dc7dc6e3b85a87da201017c734f
fajarwindows10 <firmansyah@gmail.com>
2020-03-01 13:14:28 +0700

    'new'

--- app/Http/Controllers/LaradevController.php
+++ app/Http/Controllers/LaradevController.php
@@ -587,7 +587,9 @@ class LaradevController extends Controller
                                 $string = explode(') is not present in table "', $string);
                                 $id = $string[0];
                                 $table = explode('"',$string[1])[0];
-                                $sample = DB::table($table)->first(); $sample->id = $id;
+                                $sample = DB::table($table)->first();
+                                return response()->json((array)$sample,400);
+                                $sample->id = $id;
                                 DB::table($table)->insert((array) $sample);
                                 goto addFK;
                             }else{
