59ae2999539ec102227d9b6e1f6e52f76ad91797
fajarwindows10 <firmansyah@gmail.com>
2020-04-08 12:41:30 +0700

    new

--- app/Http/Controllers/ApiFixedController.php
+++ app/Http/Controllers/ApiFixedController.php
@@ -419,7 +419,7 @@ class ApiFixedController extends Controller
             $this->success[] = "SUCCESS: data created in ".$model->getTable()." new id: $finalModel->id";
             foreach( $data as $key => $value ){
                 if(is_array($value) && count($value)>0 && in_array($key, $detailsArray) ){                
-                    $this->createOperation($key, $value, $finalModel->id, $model->table);
+                    $this->createOperation($key, $value, $finalModel->id, $model->getTable());
                 }
             }
         }
@@ -537,7 +537,7 @@ class ApiFixedController extends Controller
         $this->success[] = "SUCCESS: data update in ".$model->getTable()." id: $id";
         foreach( $data as $key => $value ){
             if(is_array($value) && count($value)>0 && in_array($key, $detailsArray) ){                
-                $this->createOperation($key, $value, $id, $model->table);
+                $this->createOperation($key, $value, $id, $model->getTable());
             }
         }
         $model = null;  
