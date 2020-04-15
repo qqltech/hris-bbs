afb0bad2cceb8ddfa93adfa35754c002c5a4c7ea
fajarwindows10 <firmansyah@gmail.com>
2020-04-15 14:25:22 +0700

    new

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -1548,6 +1548,7 @@ function _customGetData($model,$params)
         }
         $data = array_merge([
             "data"=>$fixedData],[
+            "metaScript"=>method_exists( $model, "metaScriptList" )?$model->metaScriptList():null,
             "total"=>$data->total(),
             "current_page"=>$data->currentPage(),
             "per_page"=>$data->perPage(),
