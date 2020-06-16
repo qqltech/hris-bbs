396e734ba8e0d8b855faf9efa26a1db50cd538c8
fajar <fajar@fajar.com>
2020-06-16 10:43:41 +0700

    new

--- winpush.bat
+++ winpush.bat
@@ -3,7 +3,7 @@ git commit -m 'new'
 REM git push origin master
 git push sisthaserver master
 git push igaserver master
-git push efiserverproduction master
+git push efiserver master
 git push aaa master
 REM git push hsiserver master
 REM git push kmnuserver master
