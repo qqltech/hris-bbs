8e84b5f308029f2d4497f735f81b208bf1b8c249
fajar <fajar@fajar.com>
2020-08-14 16:08:32 +0700

    new

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -1810,15 +1810,13 @@ function _customFind($model, $params)
         $data[count($detailArray)==1? $detail : $detailArray[1] ]  = $model->customGet($p);
     }
     
-    foreach($data as $i => $row){
-        $keys=array_keys($row);
-        foreach($keys as $key){
-            if( count(explode(".", $key))>2 ){
-                $newKeyArray = explode(".", $key);
-                $newKey = $newKeyArray[1].".".$newKeyArray[2];
-                $data[$i][$newKey] = $data[$i][$key];
-                unset($data[$i][$key]);
-            }
+    $keys   =   array_keys($data);
+    foreach($keys as $key){
+        if( count(explode(".", $key))>2 ){
+            $newKeyArray = explode(".", $key);
+            $newKey = $newKeyArray[1].".".$newKeyArray[2];
+            $data[$i][$newKey] = $data[$i][$key];
+            unset($data[$i][$key]);
         }
     }
     return $data;
