2bbacee0f858f00374ab6e7faeb5632bde92f7c7
fajar <fajar@fajar.com>
2020-10-06 09:42:31 +0700

    'new'

--- app/Models/Defaults/User.php
+++ app/Models/Defaults/User.php
@@ -7,10 +7,9 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use Laravel\Passport\HasApiTokens;
-class User extends Model implements AuthenticatableContract, AuthorizableContract,MustVerifyEmail
+class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use HasApiTokens, Authenticatable, Authorizable;
     protected $table = 'default_users';
@@ -18,8 +17,5 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     
     protected $hidden = [
         'password',
-    ];
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
