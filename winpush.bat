97d96b718c461b4737e9d0508dfa138834d0ffe8
fajar <fajar@fajar.com>
2020-12-03 21:50:15 +0700

    'new'

--- winpush.bat
+++ winpush.bat
@@ -1,10 +1,10 @@
 git add .
 git commit -m 'new'
-REM git push origin master
+git push origin master
 git push aaa master
 git push sisthaserver master
 git push igaserver master
-git push efiserver master
+REM git push efiserver master
 git push dejozzlarahan master
 git push pciserver master
 git push pat master
