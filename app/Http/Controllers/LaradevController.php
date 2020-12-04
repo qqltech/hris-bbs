cb7c7bc27fed01204323a00af228cec8630b2123
fajar <fajar@fajar.com>
2020-12-04 16:50:34 +0700

    new

--- app/Http/Controllers/LaradevController.php
+++ app/Http/Controllers/LaradevController.php
@@ -1373,7 +1373,7 @@ class LaradevController extends Controller
                 $commit = "first time";
                 passthru("cd $realpath; git init .;");//cd $realpath;git init .; pwd; git remote add origin $giturl");
                 sleep(2);
-                $a = passthru("cd $realpath;git remote add origin $giturl;git remote -v;");
+                $a = passthru("cd $realpath;echo $giturl;");
                 return $a;
             }
             return 'a';
