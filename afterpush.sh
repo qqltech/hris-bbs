2aa18a5cf2334758c4dee3c5a43024a6096ed0ac
fajar <fajar@fajar.com>
2020-12-04 17:00:37 +0700

    new

--- afterpush.sh
+++ afterpush.sh
@@ -1,12 +1,11 @@
 chmod 777 .gitignore
 if [ -d .git ]; then
-    mv gitignore.txt .gitignore
     git add .
     git commit -m '[Fajar Maintain Kode]'
     git push origin master
     echo 'git push dijalankan'
-fi
-# else
-#   git add .
-#   git commit -m '[Fajar Maintain Kode]'
-# fi;
+# fi
+else
+  git add .
+  git commit -m '[Fajar Maintain Kode]'
+fi;
