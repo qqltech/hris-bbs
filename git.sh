d3831e2507e9b1f574df7aba4ef12b4d733bd1e4
fajar <fajar@fajar.com>
2020-12-04 08:20:48 +0700

    'new'

---
+++ git.sh
@@ -0,0 +1,12 @@
+echo "$(cat gitignore.txt)" > .gitignore
+if [ -d .git ]; then
+    echo git dir ok;
+else
+  git init .
+  git remote add origin $1
+  git remote -v
+fi;
+git add $2
+git commit -m $3
+git push origin master
+# . git.sh https://gitlab.com/starlight93/testingfajar.git filename commitan
