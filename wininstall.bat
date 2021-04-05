90ee97594e7f76eb49b4d2c38ce81993d77dbd11
Fajar Firmansyah <mail.firmansyah93@gmail.com>
2021-04-06 02:26:27 +0700

    update supervisor dan env-example

--- wininstall.bat
+++
@@ -1,5 +0,0 @@
-CALL composer install
-CALL md database/migrations/projects
-CALL database/migrations/alters
-CALL database/migrations/__defaults/*.* database/migrations/projects
-CALL copy .env-example .env
