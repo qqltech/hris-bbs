8adc8b139b6f24358f96af43e91dda0147ff8e7d
Fajar Firmansyah <mail.firmansyah93@gmail.com>
2021-08-20 04:37:53 +0700

    update timezone

--- config/app.php
+++ config/app.php
@@ -1,2 +1,7 @@
 <?php 
-return ['timezone' => env("APP_TIMEZONE","Asia/Jakarta")];
+return [
+    'timezone' => env("APP_TIMEZONE","Asia/Jakarta"),
+
+    'key' => '32 characters',
+    'cipher' => env('APP_CIPHER','AES-256-CBC'),
+];
