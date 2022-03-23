67e021828317fbb78dda22c39d93a17244e30c12
Fajar Firmansyah <mail.firmansyah93@gmail.com>
2022-03-23 08:19:56 +0700

    fix: public route to json

--- routes/public.php
+++ routes/public.php
@@ -4,12 +4,14 @@ $router->group(['prefix'=>'public'], function () use ($router) {
     $router->get('/{modelname}/{function}', function(Request $req,$modelname,$function){
         $modelCandidate = "\App\Models\CustomModels\\$modelname";
         if( !class_exists( $modelCandidate ) ){
-            return response()->json("Model [$modelname] does not exist",404);
+            return response()->json("Resource [$modelname] does not exist",404);
         }
         $function = "public_".$function;
         $model = new $modelCandidate;
         if( !method_exists( $model, $function ) ){
-            return response()->json("function [$function] in Model [$modelname] does not exist",400);
+            return response()->json([
+                'message' => "function [$function] in Resource [$modelname] does not exist"
+            ],404);
         }
         $result = $model->$function($req);
         return $result;
@@ -17,12 +19,16 @@ $router->group(['prefix'=>'public'], function () use ($router) {
     $router->post('/{modelname}/{function}', function(Request $req,$modelname,$function){
         $modelCandidate = "\App\Models\CustomModels\\$modelname";
         if( !class_exists( $modelCandidate ) ){
-            return response()->json("Model [$modelname] does not exist",404);
+            return response()->json([
+                'message' => "Resource [$modelname] does not exist"
+            ],400);
         }
         $function = "public_".$function;
         $model = new $modelCandidate;
         if( !method_exists( $model, $function ) ){
-            return response()->json("function [$function] in Model [$modelname] does not exist",400);
+            return response()->json([
+                'message' => "function [$function] in Resource [$modelname] does not exist"
+            ],400);
         }
         $result = $model->$function($req);
         return $result;
