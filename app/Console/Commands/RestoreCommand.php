7da2562045843b315bf0a175799596d6acfba092
Fajar Firmansyah <mail.firmansyah93@gmail.com>
2022-04-10 10:46:05 +0700

    feat: restore with db

--- app/Console/Commands/RestoreCommand.php
+++ app/Console/Commands/RestoreCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use File;
 use Exception;
 use Illuminate\Console\Command;
+use App\Http\Controllers\LaradevController as Laradev;
 /**
  * Class deletePostsCommand
  *
@@ -18,7 +19,7 @@ class RestoreCommand extends Command
      *
      * @var string
      */
-    protected $signature = "restore {--src=} {--with-upload}";
+    protected $signature = "restore {--src=} {--with-upload} {--migrate}";
 
     /**
      * The console command description.
@@ -37,6 +38,7 @@ class RestoreCommand extends Command
     {
         $path = $this->option('src')??(env('BACKUP_PATH')?env('BACKUP_PATH'):base_path('app_generated_backup'));
         $withUpload = $this->option('with-upload');
+        $withMigrate = $this->option('migrate');
 
         try {
             umask(0000);
@@ -49,7 +51,7 @@ class RestoreCommand extends Command
             File::copyDirectory( "$path/tests", base_path('tests') ); // tests
             File::copyDirectory( "$path/database/migrations/projects", database_path('migrations/projects') ); //   migrations
             File::copyDirectory( "$path/database/migrations/alters", database_path('migrations/alters') );  //  alters
-            File::put( base_path(".env"), File::get("$path/env") ); // .env
+            // File::put( base_path(".env"), File::get("$path/env") ); // .env
             File::copyDirectory(  "$path/public/js", public_path('js') ); // public/js
 
             if($withUpload){
@@ -57,23 +59,36 @@ class RestoreCommand extends Command
             }
 
             File::copyDirectory( "$path/resources/views/projects", resource_path('views/projects') );   //  views/projects
+            umask(0000);
+            (new Laradev)->createModels( new \Illuminate\Http\Request );
 
-            // $host = env('DB_HOST');
-            // $username = env('DB_USERNAME');
-            // $password = env('DB_PASSWORD');
-            // $database = env('DB_DATABASE');
-            
-            // $pathFile = "$path/sqldump/";
-            // // --column-statistics=0
-            // $command = sprintf('mysql -h %s -u %s -p\'%s\' %s < %s', 
-            //             $host, 
-            //             $username, 
-            //             $password, 
-            //             $database, 
-            //             "$path/sqldump/$file");
-            
-            // exec($command);
-            
+            if( $withMigrate ){
+                //  migrate to schema
+                $host = env('DB_HOST');
+                $username = env('DB_USERNAME');
+                $password = env('DB_PASSWORD');
+                $database = env('DB_DATABASE');
+                
+                $pathFile = "$path/sqldump/";
+                $files  = scandir("$path/sqldump", SCANDIR_SORT_DESCENDING);
+                
+                if( count($files)==0 ){
+                    trigger_error("files have been restored, but no .sql file to be migrated.");
+                }
+    
+                $file = $files[0];
+                // --column-statistics=0
+                $command = sprintf('mysql -h %s -u %s -p\'%s\' %s < %s', 
+                            $host, 
+                            $username, 
+                            $password, 
+                            $database, 
+                            "$path/sqldump/$file");
+                
+                exec( $command );
+                $this->info( "migrated_sql:$file" );
+            }
+
         } catch (Exception $e) {
             $this->error($e->getMessage());
             return;
