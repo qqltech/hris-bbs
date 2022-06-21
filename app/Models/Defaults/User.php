114a7e8a656c6e1e3cb75b5f3553156eb5655546
Starlight93 <mail.starlight93@gmail.com>
2022-06-22 06:37:53 +0700

    feat: getFromHeaderToken

--- app/Models/Defaults/User.php
+++ app/Models/Defaults/User.php
@@ -24,16 +24,20 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     public function getFromHeaderToken( $bearerToken = null ){
         $access_token = $bearerToken ?? app()->request->header('Authorization');
         if(!$access_token) return null;
-        $auth_header = explode(' ', $access_token);
-        $token = $auth_header[1];
-        $token_parts = explode('.', $token);
-        $token_header = $token_parts[1];
-        $token_header_json = base64_decode($token_header);
-        $token_header_array = json_decode($token_header_json, true);
-        $accessUser = \Laravel\Passport\Token::find($token_header_array['jti']);
-        if( !$accessUser || ($accessUser && $accessUser->revoked) ){
+        try{
+            $auth_header = explode(' ', $access_token);
+            $token = $auth_header[1];
+            $token_parts = explode('.', $token);
+            $token_header = $token_parts[1];
+            $token_header_json = base64_decode($token_header);
+            $token_header_array = json_decode($token_header_json, true);
+            $accessUser = \Laravel\Passport\Token::find($token_header_array['jti']);
+            if( !$accessUser || ($accessUser && $accessUser->revoked) ){
+                return null;
+            }
+            return $this->find($accessUser->user_id);
+        }catch(\Exception $e){
             return null;
         }
-        return $this->find($accessUser->user_id);
     }
 }
