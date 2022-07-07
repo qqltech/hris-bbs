9855b5a78705e36c2984f8351c6264b4c15e7f40
Starlight93 <mail.starlight93@gmail.com>
2022-07-07 23:01:48 +0700

    feat: lite api

--- routes/operation-lite.php
+++ routes/operation-lite.php
@@ -5,5 +5,10 @@ $router->group(['prefix'=>'lite'], function () use ($router) {
     $router->group(['middleware'=>'auth'], function () use ($router) {
         $router->get('/{name}',['as'=>'read_list_native', 'uses'=> 'ApiNativeController@index']);
         $router->get('/{name}/{id}',['as'=>'read_row_native', 'uses'=> 'ApiNativeController@index']);
+        
+        $router->post('/{name}',['as'=>'create_native', 'uses'=> 'ApiNativeController@store']);
+        $router->put('/{name}/{id}',['as'=>'update_native', 'uses'=> 'ApiNativeController@update']);
+        $router->delete('/{name}/{id}',['as'=>'delete_native', 'uses'=> 'ApiNativeController@destroy']);
+
     });
 });
