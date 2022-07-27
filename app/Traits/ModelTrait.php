23779da1c82893f07bad5b6ddfabd5acf77551a3
Starlight93 <mail.starlight93@gmail.com>
2022-07-27 23:36:00 +0700

    fix: scope except GET

--- app/Traits/ModelTrait.php
+++ app/Traits/ModelTrait.php
@@ -113,7 +113,6 @@ trait ModelTrait {
                 $column = str_replace("if_", "", $key);
                 $val = strtolower($val);
                 $val = str_replace( [ '\\','(',')' ],[ '\\\\','\(','\)' ], $val);
-                $val = $val=='null'? null : $val;
                 $q->where( $column, $fixedOperator, $val );
             }
         });
