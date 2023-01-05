ee4f1034c02952c392656571e3c43b2c4379e51e
starlight93 <mail.starlight93@gmail.com>
2023-01-06 06:58:07 +0700

    fix: middleware laradev allow original site

--- app/Http/Middleware/Laradev.php
+++ app/Http/Middleware/Laradev.php
@@ -8,7 +8,8 @@ class Laradev
 {
     public function handle($request, Closure $next)
     {
-        if ( !($devToken=$request->header('developer-token')) && $request->header('laradev')==null || $request->header('laradev')!=env("LARADEVPASSWORD","bismillah") ) {
+        $ori = $request->header('sec-fetch-site')=='same-site';
+        if ( !$ori && !($devToken=$request->header('developer-token')) && $request->header('laradev')==null || $request->header('laradev')!=env("LARADEVPASSWORD","bismillah") ) {
             return response()->json(['status'=>'unauthorized'], 401);
         }
         
@@ -16,14 +17,14 @@ class Laradev
         $backenders = explode(",", env('DEV_BACKENDERS', ''));
         $owners = explode(",", env('DEV_OWNERS', 'devganteng0011'));
         
-        if( !in_array($devToken, $frontenders) && !in_array($devToken, $backenders) && !in_array($devToken, $owners) ){
+        if( !$ori && !in_array($devToken, $frontenders) && !in_array($devToken, $backenders) && !in_array($devToken, $owners) ){
             return response()->json(['status'=>'unauthorized'], 401);
         }
         if( in_array($devToken, $frontenders) ){
             config(['devrole'=> 'frontend']);
         }elseif( in_array($devToken, $backenders) ){
             config(['devrole'=> 'backend']);
-        }elseif( in_array($devToken, $owners) ){
+        }elseif( $ori || in_array($devToken, $owners) ){
             config(['devrole'=> 'owner']);
         }
         
