42823c02c3ba5f42c58c944ad4a42402e469bca0
starlight93 <mail.starlight93@gmail.com>
2023-02-01 14:31:06 +0700

    feat: generate:model artisan

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         \App\Console\Commands\BackupCommand::class,
         \App\Console\Commands\RestoreCommand::class,
+        \App\Console\Commands\GenerateModelsCommand::class
         // \KitLoong\MigrationsGenerator\MigrateGenerateCommand::class
     ];
 
