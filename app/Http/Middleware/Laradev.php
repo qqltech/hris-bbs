fc109200bfd78f9eb4d025d3649213acbac72934
Your Name <you@example.com>
2023-02-13 09:02:26 +0700

    fix: bug token developer

--- app/Http/Middleware/Laradev.php
+++ app/Http/Middleware/Laradev.php
@@ -9,10 +9,11 @@ class Laradev
     public function handle($request, Closure $next)
     {
         $ori = ($request->header('sec-fetch-site')??'same-site')=='same-site';
-        if ( !$ori && !($devToken=$request->header('developer-token')) && $request->header('laradev')==null || $request->header('laradev')!=env("LARADEVPASSWORD","bismillah") ) {
+        if ( !($devToken=$request->header('developer-token')) && !$ori && 
+$request->header('laradev')==null || $request->header('laradev')!=env("LARADEVPASSWORD","bismillah") ) {
             return response()->json(['status'=>'unauthorized'], 401);
         }
-        if($ori) $devToken = '';
+        // if($ori) $devToken = '';
         
         $frontenders = explode(",", env('DEV_FRONTENDERS', ''));
         $backenders = explode(",", env('DEV_BACKENDERS', ''));
