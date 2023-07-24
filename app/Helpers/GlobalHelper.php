a207dffd120646a57938fa0a0066caf8e362ab52
starlight93 <mail.firmansyah93@gmail.com>
2023-07-24 09:16:32 +0700

    fix: hidden full token developer

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -1929,14 +1929,14 @@ function getDeveloperActivities( $html=true ){
         if(@$act['diff'] && @$act['id']){
             $fileUrl = "<a href='/docs/activities/{$act['id']}'>{$act['file']}</a>";
         }
-        $dev = $act['name'].( @$act['ip'] ? " [ ".$act['ip']." ]":"" );
+        $dev = ( @(explode('-',$act['name'], 2)[1]) ?? '*' ).( @$act['ip'] ? " [ ".$act['ip']." ]":"" );
         $row="<tr><td style='text-align:center;'>$count</td><td style='text-align:center;'>{$act['time']}</td><td>$dev</td><td>{$act['action']}</td>
         <td>$fileUrl</td></tr>";
         $htmlData.=$row;
     }
 
     return "<h3 style='text-align:center'> Dev Activities on ".env('APP_NAME')." until ".(Carbon::now()->format('d/m/Y')).'</h3><table style="width:100%;" border="1" cellpadding=1>
-            <thead style="background:pink;"><th>No</th><th>Time</th><th>Dev Token</th><th>Action</th><th>Relation</th></thead>'.
+            <thead style="background:pink;"><th>No</th><th>Time</th><th>Developer</th><th>Action</th><th>Relation</th></thead>'.
             "<tbody>$htmlData</tbody></table>";
 }
 
